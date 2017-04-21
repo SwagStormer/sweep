1d67e241f5865518754abb931c6d0dcbe19fe3d4
SwagStormer <ryan@ryansbees.com>
2017-04-21 08:17:40 -0600

    routes generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,24 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './home/routes/courses/courses.component';
+import { CourseComponent } from './home/routes/course/course.component';
+import { AssignmentComponent } from './home/routes/assignment/assignment.component';
+import { GradeComponent } from './home/routes/grade/grade.component';
+import { AssignmentsComponent } from './home/routes/assignments/assignments.component';
+import { DashboardComponent } from './home/routes/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    CoursesComponent,
+    CourseComponent,
+    AssignmentComponent,
+    GradeComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,23 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './home/routes/dashboard/dashboard.component';
+import { CourseComponent } from './home/routes/course/course.component';
+import { GradeComponent } from './home/routes/grade/grade.component';
+import { AssignmentsComponent } from './home/routes/assignments/assignments.component';
+import { AssignmentComponent } from './home/routes/assignment/assignment.component';
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'course', component: CourseComponent },
+      { path: 'grade', component: GradeComponent },
+      { path: 'assignments', component: AssignmentsComponent },
+      { path: 'assignment', component: AssignmentComponent }
+    ]
   }
 ];
 @NgModule({
